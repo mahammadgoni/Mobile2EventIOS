dec484053920441b3f5e947ec9ab62ee3443a1eb
Goni <goni@github.com>
2018-06-22 12:39:25 +0530

    Base Set up Changes

---
+++ src/com/BaseSetup/BaseSetUp.java
@@ -0,0 +1,156 @@
+package com.BaseSetup;
+
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseSetUp {
+	
+  protected WebDriver driver;
+
+	By emailId = By.id("ws.e2m.main:id/et_email");
+	
+	By pass = By.id("ws.e2m.main:id/et_password");
+	
+	By proceedBtn1 = By.id("ws.e2m.main:id/tv_proceed");
+	
+	By proceedBtn2 = By.id("ws.e2m.main:id/rl_proceed");
+	
+	By forgotPassBtn = By.id("ws.e2m.main:id/tv_forgot_pswd");
+	
+	By submitBtn = By.id("ws.e2m.main:id/tv_submit");
+	
+	By menu = By.id("ws.e2m.main:id/btn_home");
+  
+  
+  
+    public BaseSetUp(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected void waitForVisibilityOf(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+    
+    protected void waitForClickabilityOf(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, 120);
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+    
+//	Common Login Method
+	
+	public void commonActivity(String userName,String password) throws InterruptedException{
+		
+		System.out.println("Clicking on Your Email ");
+
+		waitForClickabilityOf(emailId);
+
+		driver.findElement(emailId).clear();
+
+		System.out.println("Entering the Email  :" + userName);
+
+		driver.findElement(emailId).sendKeys(userName);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Proceed Button ");
+
+		waitForClickabilityOf(proceedBtn1);
+
+		driver.findElement(proceedBtn1).click();
+		
+		Thread.sleep(2000);
+
+		System.out.println("Entering the Pin  :" + password);
+
+		waitForClickabilityOf(pass);
+		
+		driver.findElement(pass).clear();
+
+		driver.findElement(pass).sendKeys(password);
+		
+		Thread.sleep(2000);
+		
+		System.out.println("Clicking on Proceed Button ");
+
+		waitForClickabilityOf(proceedBtn2);
+
+		driver.findElement(proceedBtn2).click();
+		
+		Thread.sleep(2000);
+		
+		
+	}
+    
+
+    public void scrollPageUp() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.50);
+        swipeObject.put("startY", 0.95);
+        swipeObject.put("endX", 0.50);
+        swipeObject.put("endY", 0.01);
+        swipeObject.put("duration", 3.0);
+        js.executeScript("mobile: swipe", swipeObject);
+    }
+
+
+    public void swipeLeftToRight() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.01);
+        swipeObject.put("startY", 0.5);
+        swipeObject.put("endX", 0.9);
+        swipeObject.put("endY", 0.6);
+        swipeObject.put("duration", 3.0);
+        js.executeScript("mobile: swipe", swipeObject);
+    }
+
+    public void swipeRightToLeft() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.9);
+        swipeObject.put("startY", 0.5);
+        swipeObject.put("endX", 0.01);
+        swipeObject.put("endY", 0.5);
+        swipeObject.put("duration", 3.0);
+        js.executeScript("mobile: swipe", swipeObject);
+    }
+
+    public void swipeFirstCarouselFromRightToLeft() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> swipeObject = new HashMap<String, Double>();
+        swipeObject.put("startX", 0.9);
+        swipeObject.put("startY", 0.2);
+        swipeObject.put("endX", 0.01);
+        swipeObject.put("endY", 0.2);
+        swipeObject.put("duration", 3.0);
+        js.executeScript("mobile: swipe", swipeObject);
+    }
+
+    public void performTapAction(WebElement elementToTap) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        HashMap<String, Double> tapObject = new HashMap<String, Double>();
+        tapObject.put("x", (double) 360); // in pixels from left
+        tapObject.put("y", (double) 170); // in pixels from top
+        tapObject.put("element", Double.valueOf(((RemoteWebElement) elementToTap).getId()));
+        js.executeScript("mobile: tap", tapObject);
+    }
+    
+    public void highLightAnElement(By element){
+    	
+    	for (int i = 0; i <2; i++) {
+            JavascriptExecutor js = (JavascriptExecutor)driver;
+            js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "color: yellow; border: 2px solid yellow;");
+            js.executeScript("arguments[0].setAttribute('style', arguments[1]);", element, "");
+            }
+        }
+
+}
